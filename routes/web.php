429f1cadeed610011e9e5b993db4e72b8b89aac8
Maksimilian <glytop@mail.ru>
2024-06-05 10:02:26 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+Route::get('/', \App\Http\Controllers\Main\IndexController::class)->name('main.index');
+
+Route::group(['prefix' => 'categories'], function () {
+    Route::get('/', \App\Http\Controllers\Category\IndexController::class)->name('category.index');
+    Route::get('/create', \App\Http\Controllers\Category\CreateController::class)->name('category.create');
+    Route::post('/', \App\Http\Controllers\Category\StoreController::class)->name('category.store');
+    Route::get('/{category}/edit', \App\Http\Controllers\Category\EditController::class)->name('category.edit');
+    Route::get('/{category}', \App\Http\Controllers\Category\ShowController::class)->name('category.show');
+    Route::patch('/{category}', \App\Http\Controllers\Category\UpdateController::class)->name('category.update');
+    Route::delete('/{category}', \App\Http\Controllers\Category\DeleteController::class)->name('category.delete');
+});
