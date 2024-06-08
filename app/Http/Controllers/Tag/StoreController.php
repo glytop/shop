87bf5bb267cd594d3a0ae7cd82c62f1483493270
Maksimilian <glytop@mail.ru>
2024-06-08 10:31:24 +0300

    CRUD user

--- app/Http/Controllers/Tag/StoreController.php
+++ app/Http/Controllers/Tag/StoreController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Tag;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Category\StoreRequest;
+use App\Http\Requests\Tag\StoreRequest;
 use App\Models\Tag;
 
 class StoreController extends Controller
--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,22 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const GENDER_MALE = 1;
+    const GENDER_FEMALE = 2;
+
+    protected $table = 'users';
+    protected $guarded = false;
+
+    static function getGenders(){
+        return [
+            self::GENDER_MALE => 'Мужской',
+            self::GENDER_FEMALE => 'Женский',
+        ];
+    }
+
+    public function getGenderTitleAttribute(){
+        return self::getGenders()[$this->gender];
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -19,8 +35,13 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
+        'patronymic',
+        'age',
+        'gender',
+        'address'
     ];
 
     /**
